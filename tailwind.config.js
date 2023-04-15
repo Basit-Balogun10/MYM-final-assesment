a0e8376238efd856130f2092427a846a1ae5a7db
Basit Balogun <basitbalogun10@gmail.com>
2023-04-15 20:40:50 +0100

    Initialized app and grabbed assets (PNGs and SVGs) from Figma file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                greenBg: "#C2ECC1",
+                greenText: "#608662",
+                blueBg: "#C5C7F6",
+                grey: "#F8F8F8",
+                
+            },
+            fontFamily: {
+                sulphurPoint: ["Sulphur Point", "sans - serif"],
+            },
+        },
+    },
+    plugins: [],
+};
