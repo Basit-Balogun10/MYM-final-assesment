017fb52824da0a86d5e64e474926a821f3bd9edc
Basit Balogun <basitbalogun10@gmail.com>
2023-04-17 08:44:49 +0100

    displayed minimal plan upgrade div for mobile screens on the side menu

--- src/containers/SideMenu.tsx
+++ src/containers/SideMenu.tsx
@@ -29,7 +29,9 @@ const SideMenu = () => {
         <section className="basis-[40%] md:basis-[20%] xl:basis-[15%] md:w-[20%] flex-1 lg:flex-none grow shrink-0 lg:shrink-[initial] py-6 space-y-4 bg-black text-grayBg">
             {/* LOGO */}
             <div className="w-3/4 md:w-1/2 ml-10 sm:ml-14 md:mx-auto mb-6 flex items-center md:justify-center space-x-2">
-                <div className="flex-initial"><Logo /></div>
+                <div className="flex-initial">
+                    <Logo />
+                </div>
                 <h1 className="md:text-sm">Realply</h1>
             </div>
             {/* NAVIGATION MENU */}
@@ -37,7 +39,7 @@ const SideMenu = () => {
                 {navigationMenuItems.map((item, index) => (
                     <SideMenuItem
                         key={index}
-                        additionalStyles={` ${index === 4 ? "mt-72 sm:mt-9" : "mt-1"}`}
+                        additionalStyles={` ${index === 4 ? "mt-20 sm:mt-9" : "mt-1"}`}
                         isActive={item.isActive}
                         text={item.name}
                     >
@@ -46,18 +48,18 @@ const SideMenu = () => {
                 ))}
             </div>
             {/* PLAN UPGRADE NOTICE */}
-            <div className="hidden sm:block w-4/5 space-y-2 p-4 mx-auto border border-white/30 rounded-3xl">
-                <div className="font-roboto">
+            <div className="w-4/5 space-y-2 p-4 mx-auto border border-white/30 rounded-3xl">
+                <div className="text-center sm:text-left font-roboto">
                     <h3 className="text-grayBg text-xs font-bold">
                         Your free trial is about to end in
                     </h3>
                     <h3 className="text-grayBg text-lg font-bold">10 days.</h3>
                 </div>
                 <div className="space-y-2">
-                    <p className="text-slate-400 text-[0.6rem] leading-tight">
+                    <p className="text-center sm:text-left text-slate-400 text-[0.6rem] leading-tight">
                         You will not be billed during your free trial
                     </p>
-                    <p className="text-slate-400 text-[0.6rem] leading-tight">
+                    <p className="hidden sm:block text-slate-400 text-[0.6rem] leading-tight">
                         You will not be billed during your free trial.To keep
                         your projects running after the trial end, upgrade to a
                         paid option.
